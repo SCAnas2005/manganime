c993647d1988716af0c059c6441a941be749c37a
Anas Hadri <haanas2020@gmail.com>
2025-11-17 17:26:36 +0100

    ajout providers

---
+++ lib/providers/like_storage.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/models/anime_detail.dart';
+import 'package:hive/hive.dart';
+
+class LikeStorage {
+  static const String LIKED_ANIMES_KEY = "liked_animes";
+  static final Box<List> _box = Hive.box<List>("likes_box");
+
+  static List<int> getIdAnimeLiked() {
+    final list = _box.get("liked_animes", defaultValue: [])!;
+    return List<int>.from(list);
+  }
+
+  static Future<void> toggleAnimeLike(int animeId) async {
+    final liked = getIdAnimeLiked();
+
+    if (liked.contains(animeId)) {
+      liked.remove(animeId);
+    } else {
+      liked.add(animeId);
+    }
+
+    await _box.put(LIKED_ANIMES_KEY, liked);
+  }
+
+  static bool isLiked(int animeId) {
+    return getIdAnimeLiked().contains(animeId);
+  }
+}
