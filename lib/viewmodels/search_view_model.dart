1a5329bd249c9977bbb772131a964c521493a69c
Anas Hadri <haanas2020@gmail.com>
2025-11-21 16:17:55 +0100

    merge ilario branch

--- lib/viewmodels/search_view_model.dart
+++ lib/viewmodels/search_view_model.dart
@@ -3,9 +3,6 @@ import 'package:flutter_application_1/models/anime.dart';
 import 'package:flutter_application_1/services/jikan_service.dart';
 import 'package:flutter_application_1/widgets/Search.dart';
 
-
 class serachViewModel extends ChangeNotifier {
-    final JikanService _service = JikanService();
-    
-    
+  final JikanService _service = JikanService();
 }
