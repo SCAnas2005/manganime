f3b3501e2cb3574f4c531e9ab32cb8d99ae151b3
= <=>
2025-11-22 11:22:55 +0100

    Refactorisation du code pour la page favoris avec ajout de factory dans anime.dart

--- lib/views/favorite_anime_view.dart
+++ lib/views/favorite_anime_view.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/models/anime.dart';
 import 'package:flutter_application_1/viewmodels/favorite_view_model.dart';
+import 'package:flutter_application_1/widgets/anime_card.dart';
+import 'package:flutter_application_1/widgets/anime_list_item.dart';
 import 'package:flutter_application_1/widgets/like_widget/like_button.dart';
 import 'package:provider/provider.dart';
 
@@ -18,20 +21,37 @@ class FavoriteAnimeView extends StatelessWidget {
       return const Center(child: Text("Aucun favori pour le moment"));
     }
 
-    return ListView.builder(
+    // return ListView.builder(
+    //   itemCount: vm.favoris.length,
+    //   itemBuilder: (context, index) {
+    //     final anime = Anime.fromDetail(vm.favoris[index]);
+
+    //     return AnimeListItem(
+    //       anime: anime,
+    //       onTap: () => {},
+    //       isLiked: true,
+    //       onLikeToggle: () => vm.removeFavoris(anime.id),
+    //     );
+    //   },
+    // );
+
+    return GridView.builder(
+      padding: const EdgeInsets.all(12),
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        mainAxisSpacing: 12,
+        crossAxisSpacing: 12,
+        childAspectRatio: 0.7,
+      ),
       itemCount: vm.favoris.length,
       itemBuilder: (context, index) {
-        final anime = vm.favoris[index];
+        final anime = Anime.fromDetail(vm.favoris[index]);
 
-        return ListTile(
-          leading: Image.network(anime.imageUrl),
-          title: Text(anime.title),
-          subtitle: Text("Score : ${anime.score}"),
-          trailing: LikeButton(
-            isLiked: true,
-            onTap: () => vm.removeFavoris(anime.id),
-            iconSize: 30,
-          ),
+        return AnimeCard(
+          anime: anime,
+          onTap: (anime) =>
+              {}, // Test console : print('Tapped ${anime.title}'),
+          onLikeDoubleTap: (anime) => vm.removeFavoris(anime.id),
         );
       },
     );
