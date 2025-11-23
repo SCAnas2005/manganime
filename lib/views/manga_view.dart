5ca777929276f39001e43fea263590a803d6f1b1
= <=>
2025-11-23 09:13:26 +0100

    Refactorisation du code pour la page animes et mangas + création de TabSection et TabSwitcher

--- lib/views/manga_view.dart
+++ lib/views/manga_view.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_application_1/models/manga.dart';
 import 'package:flutter_application_1/viewmodels/manga_view_model.dart';
 import 'package:flutter_application_1/widgets/manga_card.dart';
+import 'package:flutter_application_1/widgets/ui/tab_section.dart';
+import 'package:flutter_application_1/widgets/ui/tab_switcher.dart';
 import 'package:provider/provider.dart';
 
 class MangaView extends StatefulWidget {
@@ -12,6 +14,8 @@ class MangaView extends StatefulWidget {
 }
 
 class _MangaViewState extends State<MangaView> {
+  int selectedTab = 0;
+
   late ScrollController _popularController;
   late ScrollController _publishingController;
   late ScrollController _mostLikedController;
@@ -69,64 +73,67 @@ class _MangaViewState extends State<MangaView> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: const [
-                  Text(
-                    "Mangas pour toi",
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
-                    ),
-                  ),
-                ],
+              TabSwitcher(
+                tabs: ["Pour toi", "Tendances"],
+                selectedIndex: selectedTab,
+                onChanged: (index) {
+                  setState(() {
+                    selectedTab = index;
+                  });
+                },
+                isEnabled: [
+                  true,
+                  false,
+                ], // mettre [true,true] quand la page tendance sera faite
               ),
               const SizedBox(height: 20),
-              const Text(
-                "Les plus populaires",
-                style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
-                ),
-              ),
-              const SizedBox(height: 10),
-              _buildHorizontalList(
-                vm.popular,
-                controller: _popularController,
-                onTap: vm.openMangaPage,
-              ),
+
+              _buildForYou(vm),
               const SizedBox(height: 20),
-              const Text(
-                "En publication",
-                style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
-                ),
-              ),
-              const SizedBox(height: 10),
-              _buildHorizontalList(
-                vm.publishing,
-                controller: _publishingController,
-                onTap: vm.openMangaPage,
-              ),
-              const SizedBox(height: 20),
-              const Text(
-                "Les plus aimés",
-                style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
-                ),
-              ),
-              const SizedBox(height: 10),
-              _buildHorizontalList(
-                vm.mostLiked,
-                controller: _mostLikedController,
-                onTap: vm.openMangaPage,
-              ),
+              // const Text(
+              //   "Les plus populaires",
+              //   style: TextStyle(
+              //     fontSize: 18,
+              //     fontWeight: FontWeight.bold,
+              //     color: Colors.white,
+              //   ),
+              // ),
+              // const SizedBox(height: 10),
+              // _buildHorizontalList(
+              //   vm.popular,
+              //   controller: _popularController,
+              //   onTap: vm.openMangaPage,
+              // ),
+              // const SizedBox(height: 20),
+              // const Text(
+              //   "En publication",
+              //   style: TextStyle(
+              //     fontSize: 18,
+              //     fontWeight: FontWeight.bold,
+              //     color: Colors.white,
+              //   ),
+              // ),
+              // const SizedBox(height: 10),
+              // _buildHorizontalList(
+              //   vm.publishing,
+              //   controller: _publishingController,
+              //   onTap: vm.openMangaPage,
+              // ),
+              // const SizedBox(height: 20),
+              // const Text(
+              //   "Les plus aimés",
+              //   style: TextStyle(
+              //     fontSize: 18,
+              //     fontWeight: FontWeight.bold,
+              //     color: Colors.white,
+              //   ),
+              // ),
+              // const SizedBox(height: 10),
+              // _buildHorizontalList(
+              //   vm.mostLiked,
+              //   controller: _mostLikedController,
+              //   onTap: vm.openMangaPage,
+              // ),
             ],
           ),
         ),
@@ -134,28 +141,68 @@ class _MangaViewState extends State<MangaView> {
     );
   }
 
-  Widget _buildHorizontalList(
-    List<Manga> mangas, {
-    Function(BuildContext context, Manga manga)? onTap,
-    ScrollController? controller,
-  }) {
-    return SizedBox(
-      height: 250,
-      child: ListView.builder(
-        controller: controller,
-        scrollDirection: Axis.horizontal,
-        itemCount: mangas.length,
-        itemBuilder: (context, index) {
-          final manga = mangas[index];
-          return Padding(
-            padding: const EdgeInsets.only(right: 10),
-            child: MangaCard(
-              manga: manga,
-              onTap: (item) => onTap?.call(context, item),
-            ),
-          );
-        },
-      ),
+  // Widget _buildHorizontalList(
+  //   List<Manga> mangas, {
+  //   Function(BuildContext context, Manga manga)? onTap,
+  //   ScrollController? controller,
+  // }) {
+  //   return SizedBox(
+  //     height: 250,
+  //     child: ListView.builder(
+  //       controller: controller,
+  //       scrollDirection: Axis.horizontal,
+  //       itemCount: mangas.length,
+  //       itemBuilder: (context, index) {
+  //         final manga = mangas[index];
+  //         return Padding(
+  //           padding: const EdgeInsets.only(right: 10),
+  //           child: MangaCard(
+  //             manga: manga,
+  //             onTap: (item) => onTap?.call(context, item),
+  //           ),
+  //         );
+  //       },
+  //     ),
+  //   );
+  // }
+
+  // Onglet 1 : Pour toi
+  Widget _buildForYou(MangaViewModel vm) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        TabSection<Manga>(
+          title: "Les plus populaires",
+          items: vm.popular,
+          controller: _popularController,
+          onTap: (manga) => vm.openMangaPage(context, manga),
+          itemBuilder: (manga) => MangaCard(
+            manga: manga,
+            onTap: (m) => vm.openMangaPage(context, m),
+          ),
+        ),
+        const SizedBox(height: 20),
+        TabSection<Manga>(
+          title: "En publication",
+          items: vm.publishing,
+          controller: _publishingController,
+          onTap: (manga) => vm.openMangaPage(context, manga),
+          itemBuilder: (manga) => MangaCard(
+            manga: manga,
+            onTap: (m) => vm.openMangaPage(context, m),
+          ),
+        ),
+        TabSection<Manga>(
+          title: "Les plus aimés",
+          items: vm.mostLiked,
+          controller: _mostLikedController,
+          onTap: (manga) => vm.openMangaPage(context, manga),
+          itemBuilder: (manga) => MangaCard(
+            manga: manga,
+            onTap: (m) => vm.openMangaPage(context, m),
+          ),
+        ),
+      ],
     );
   }
 }
