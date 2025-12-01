6e3b3abfc2fb560c18c0c89097fa95653e118012
KarimIsCoding <karim126traore@gmail.com>
2025-12-01 20:42:21 +0100

    Ajout des  Statistiques

--- lib/viewmodels/anime_stat_model.dart
+++ lib/viewmodels/anime_stat_model.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_application_1/providers/user_stats_provider.dart';
 import 'package:flutter_application_1/providers/like_storage_provider.dart';
+import 'package:flutter_application_1/providers/screen_time_provider.dart';
 
 class AnimeStatModel {
   late int viewsNumber;
@@ -10,7 +11,7 @@ class AnimeStatModel {
 
   late int viewNumber;
 
-  late int timeNumber;
+  late String timeFormatted;
 
   late Map<String,int> categoryPercentage; 
 
@@ -18,7 +19,14 @@ class AnimeStatModel {
     rankNumber = 12;
     likesNumber = LikeStorage.getIdAnimeLiked().length;
     viewNumber = UserStatsProvider.getAnimeViewsCount();
-    timeNumber = 32;
+    final int totalSeconds = ScreenTimeProvider.getTotalScreenTime();
+    final int hours = totalSeconds ~/ 3600;
+    final int minutes = (totalSeconds % 3600) ~/ 60;
+    if(hours >= 1) {
+      timeFormatted = "${hours}h${minutes}m";
+    } else {
+      timeFormatted = "${minutes} m";
+    }
     categoryPercentage = {"Action":35, "Shonen":28,"Romance":18,"Fantaisie":12,"Seinen":7};
   } 
 }
