4d7ede358f27c2f27713fbeec3da521884005579
Anas Hadri <haanas2020@gmail.com>
2025-12-22 10:49:46 +0100

    adding bootloader file for app startup

--- lib/main.dart
+++ lib/main.dart
@@ -1,52 +1,17 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/app/app.dart';
-import 'package:flutter_application_1/models/anime.dart';
-import 'package:flutter_application_1/providers/anime_cache_provider.dart';
-import 'package:flutter_application_1/providers/anime_sections_provider.dart';
-import 'package:flutter_application_1/providers/database_provider.dart';
+import 'package:flutter_application_1/providers/boot_loader.dart';
 import 'package:flutter_application_1/providers/global_anime_favorites_provider.dart';
 import 'package:flutter_application_1/providers/global_manga_favorites_provider.dart';
-import 'package:flutter_application_1/providers/like_storage_provider.dart';
-import 'package:flutter_application_1/providers/manga_cache_provider.dart';
 import 'package:flutter_application_1/providers/settings_repository_provider.dart';
 import 'package:flutter_application_1/providers/settings_storage_provider.dart';
-import 'package:flutter_application_1/providers/user_stats_provider.dart';
-import 'package:flutter_application_1/providers/screen_time_provider.dart';
-import 'package:flutter_application_1/services/jikan_service.dart';
 import 'package:flutter_application_1/viewmodels/app_settings_view_model.dart';
 import 'package:flutter_application_1/viewmodels/search_view_model.dart';
-import 'package:hive_flutter/hive_flutter.dart';
 import 'package:provider/provider.dart';
 
 Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-
-  await Hive.initFlutter();
-
-  // Ouvre une box pour la base de donnée
-  await DatabaseProvider.init();
-
-  // Ouvre une box pour les likes
-  await LikeStorage.init();
-  // Ouvre une box pour les vues
-  await UserStatsProvider.init();
-
-  // Ouvre une box pour le cache d'anime/manga
-  await AnimeCache.instance.init();
-  await MangaCache.instance.init();
-
-  // Ouvre une box pour les sections anime/manga
-  await AnimeSectionsProvider.instance.init();
-
-  // Initialisation et démarrage du suivi du temps d'écran
-  await ScreenTimeProvider.init();
-  ScreenTimeProvider().startTracking();
-
-  await SettingsStorage.instance.init();
-
-  await onAppStart();
+  await BootLoader.initAll();
+  await BootLoader.onAppStart();
 
   runApp(
     MultiProvider(
@@ -63,30 +28,4 @@ Future<void> main() async {
       child: const App(),
     ),
   );
-}
-
-Future<void> onAppStart() async {
-  var provider = SettingsRepositoryProvider(SettingsStorage.instance);
-  bool isFirstLaunch = true;
-  //provider.getSettings().isFirstLaunch;
-
-  await provider.updateSettings(
-    provider.getSettings().copyWith(isFirstLaunch: true),
-  );
-
-  if (isFirstLaunch) {
-    debugPrint("========= App first launch");
-
-    // debugPrint("Database populate started");
-    // await DatabaseProvider.instance.clear<Anime>();
-    // await DatabaseProvider.instance.populate<Anime>(JikanService(), 300);
-    // debugPrint("Updating cache");
-    // await AnimeCache.instance.updateCache();
-
-    await provider.updateSettings(
-      provider.getSettings().copyWith(isFirstLaunch: false),
-    );
-
-    // exit(0);
-  }
 }
---
+++ lib/providers/boot_loader.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/providers/anime_cache_provider.dart';
+import 'package:flutter_application_1/providers/anime_sections_provider.dart';
+import 'package:flutter_application_1/providers/database_provider.dart';
+import 'package:flutter_application_1/providers/like_storage_provider.dart';
+import 'package:flutter_application_1/providers/manga_cache_provider.dart';
+import 'package:flutter_application_1/providers/screen_time_provider.dart';
+import 'package:flutter_application_1/providers/settings_repository_provider.dart';
+import 'package:flutter_application_1/providers/settings_storage_provider.dart';
+import 'package:flutter_application_1/providers/user_stats_provider.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+
+class BootLoader {
+  static Future<void> initAll() async {
+    WidgetsFlutterBinding.ensureInitialized();
+
+    await Hive.initFlutter();
+
+    // Ouvre une box pour la base de donnée
+    await DatabaseProvider.init();
+
+    // Ouvre une box pour les likes
+    await LikeStorage.init();
+    // Ouvre une box pour les vues
+    await UserStatsProvider.init();
+
+    // Ouvre une box pour le cache d'anime/manga
+    await AnimeCache.instance.init();
+    await MangaCache.instance.init();
+
+    // Ouvre une box pour les sections anime/manga
+    await AnimeSectionsProvider.instance.init();
+
+    // Initialisation et démarrage du suivi du temps d'écran
+    await ScreenTimeProvider.init();
+    ScreenTimeProvider().startTracking();
+
+    await SettingsStorage.instance.init();
+  }
+
+  static Future<void> onAppStart() async {
+    var provider = SettingsRepositoryProvider(SettingsStorage.instance);
+    bool isFirstLaunch = true;
+    //provider.getSettings().isFirstLaunch;
+
+    await provider.updateSettings(
+      provider.getSettings().copyWith(isFirstLaunch: true),
+    );
+
+    if (isFirstLaunch) {
+      debugPrint("========= App first launch");
+
+      // debugPrint("Database populate started");
+      // await DatabaseProvider.instance.clear<Anime>();
+      // await DatabaseProvider.instance.populate<Anime>(JikanService(), 300);
+      // debugPrint("Updating cache");
+      // await AnimeCache.instance.updateCache();
+
+      await provider.updateSettings(
+        provider.getSettings().copyWith(isFirstLaunch: false),
+      );
+    }
+  }
+}
